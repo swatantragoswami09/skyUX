97b1e732d101635ba75e514adf11b82193ce2d5b
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-02-04 19:45:11 +0530

    skyux commit

---
+++ src/app/app-extras.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+
+import { AppSkyModule } from "./app-sky.module";
+
+@NgModule({
+  exports: [AppSkyModule],
+})
+export class AppExtrasModule {}
---
+++ src/app/app-sky.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+
+import { SkyAvatarModule } from "@skyux/avatar";
+
+import { SkyAlertModule, SkyKeyInfoModule } from "@skyux/indicators";
+
+import { SkyFluidGridModule, SkyCardModule } from "@skyux/layout";
+
+import { SkyNavbarModule } from "@skyux/navbar";
+
+@NgModule({
+  exports: [
+    SkyAvatarModule,
+    SkyAlertModule,
+    SkyKeyInfoModule,
+    SkyFluidGridModule,
+    SkyNavbarModule,
+    SkyCardModule,
+  ],
+})
+export class AppSkyModule {}
